e9d4a4f01d41b8bfce8c6743e1051202dccf18fb
ehiosu <53658556+ehiosu@users.noreply.github.com>
2022-09-05 21:51:40 +0100

    Base Files
    Co-authored-by: ikemonyeka <Ikemonyeka@users.noreply.github.com>

--- QuizBackend/Quizzes/admin.py
+++ QuizBackend/Quizzes/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from Quizzes.models import Answer,Question,Quiz
+
+
+admin.site.register(Answer)
+admin.site.register(Question)
+admin.site.register(Quiz)
 
 # Register your models here.
--- QuizBackend/Quizzes/models.py
+++ QuizBackend/Quizzes/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from Users.models import Teacher
+class Quiz(models.Model):
+    Organizer= models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    
+class Question(models.Model):
+    question = models.TextField()
+    quiz_id = models.ForeignKey(Quiz,on_delete=models.CASCADE)
+    Image1 = models.URLField(blank=True)
+    Image2 = models.URLField(blank=True)
+    Image3 = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.question
+
+class Answer(models.Model):
+    IsCorrect= models.BooleanField()
+    QuestionID=models.ForeignKey(Question, on_delete=models.CASCADE)
+    Answer=models.TextField(max_length=500)
+
+    def __str__(self):
+        return self.Answer
+    
+
+
 
 # Create your models here.
